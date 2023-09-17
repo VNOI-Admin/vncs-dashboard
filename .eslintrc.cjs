e9510cf3c9e23709526072d497fe3fca89f6bc0c
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-09-17 18:28:50 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/** @type {import("@types/eslint").Linter.BaseConfig} */
+module.exports = {
+	root: true,
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"prettier",
+	],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "simple-import-sort", "tailwindcss"],
+	parserOptions: {
+		sourceType: "module",
+		ecmaVersion: 2020,
+		extraFileExtensions: [".svelte"],
+		warnOnUnsupportedTypeScriptVersion: false,
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true,
+	},
+	rules: {
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/consistent-type-imports": "error",
+		"@typescript-eslint/no-unused-vars": [
+			"warn",
+			{ ignoreRestSiblings: true, varsIgnorePattern: "^(_|\\$\\$)", argsIgnorePattern: "^_" },
+		],
+		"simple-import-sort/imports": "warn",
+		"simple-import-sort/exports": "warn",
+		"tailwindcss/no-custom-classname": "off",
+		"tailwindcss/classnames-order": "off",
+	},
+	overrides: [
+		{
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+			},
+		},
+	],
+};
